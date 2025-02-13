feecd16bfdbaf7c516098deda9cc150f435b7632
wade-loccisano <wade.loccisano@gmail.com>
2025-02-12 20:49:40 -0500

    Add the add screen.

--- app/blueprints/add/routes.py
+++ app/blueprints/add/routes.py
@@ -7,24 +7,18 @@ from app.utils.decorators import validate_json
 from flask_restful import Resource, reqparse
 
 
-@bp.route("/add", methods=["GET"])
+@bp.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "GET":
-        return render_template("add.html")
+        limit = request.args.get("limit", None)
+        order_by = request.args.get("order_by", None)
 
+        decks = db.session.query(Deck).order_by(order_by).limit(limit).all()
 
-@bp.route("/add/<int:id>/", methods=["GET", "POST"])
-@validate_json()
-def add_card(id):
-    deck = db.session.query(Deck).get(id)
-    if deck is None:
-        response = f"Deck:{id} not found."
-        return Response(response=response, status=404)
-
-    if request.method == "GET":
-        return render_template("add.html")
+        return render_template("add.html", decks=decks, deckId=decks[0].id)
 
     if request.method == "POST":
+        id = request.form.get("deck")
         front = request.form.get("front")
         back = request.form.get("back")
 
@@ -33,7 +27,30 @@ def add_card(id):
         db.session.add(card)
         db.session.commit()
 
-        return 200
-        # return redirect(
-        #     url_for("decks_blueprint.cards_blueprint.cards", deck_id=deck_id)
-        # )
+        limit = request.args.get("limit", None)
+        order_by = request.args.get("order_by", None)
+
+        decks = db.session.query(Deck).order_by(order_by).limit(limit).all()
+
+        return render_template("add.html", decks=decks, deckId=id)
+
+
+# @bp.route("/add", methods=["POST"])
+# @validate_json()
+# def add_card(id):
+#     print("hi lol")
+#     deck = db.session.query(Deck).get(id)
+#     if deck is None:
+#         response = f"Deck:{id} not found."
+#         return Response(response=response, status=404)
+
+#     if request.method == "POST":
+#         front = request.form.get("front")
+#         back = request.form.get("back")
+
+#         card = Card.from_string(front, back, id)
+
+#         db.session.add(card)
+#         db.session.commit()
+
+#         return 200

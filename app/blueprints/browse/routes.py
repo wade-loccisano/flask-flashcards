f5529d01556974c8406a1002151757a271179cba
wade-loccisano <wade.loccisano@gmail.com>
2025-02-18 13:54:28 -0500

    Wire up browse screen to post endpoint.

--- app/blueprints/browse/routes.py
+++ app/blueprints/browse/routes.py
@@ -18,19 +18,20 @@ def browse():
         return render_template("browse.html", cards=cards)
 
     if request.method == "POST":
-        id = request.form.get("deck")
-        cardId = request.form.get("card")
+        id = request.form.get("deckId")
+        cardId = request.form.get("cardId")
         front = request.form.get("front")
         back = request.form.get("back")
 
-        card = Card.from_string(front, back, id)
+        card = db.session.query(Card).filter(Card.id == cardId).first()
+        card.front = front
+        card.back = back
 
-        db.session.add(card)
         db.session.commit()
 
         limit = request.args.get("limit", None)
         order_by = request.args.get("order_by", None)
 
-        decks = db.session.query(Deck).order_by(order_by).limit(limit).all()
+        cards = db.session.query(Card).order_by(order_by).limit(limit).all()
 
-        return render_template("add.html", decks=decks, deckId=id)
+        return render_template("browse.html", cards=cards)

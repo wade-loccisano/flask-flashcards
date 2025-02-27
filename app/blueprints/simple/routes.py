cdf38fe7932bd808d599b8b683b4428e5e599697
wade-loccisano <wade.loccisano@gmail.com>
2025-02-27 09:14:25 -0500

    Handle logged in users returning to splash page later.

--- app/blueprints/simple/routes.py
+++ app/blueprints/simple/routes.py
@@ -3,10 +3,22 @@ from flask import render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from app.extensions.seed import seed_database
 from app.resources.forms.signup import SignUpForm
+from app.extensions import db
+from app.models.deck import Deck
 
 
 @bp.route("/")
 def index():
+    if current_user and current_user.is_authenticated:
+        decks = db.session.query(Deck).filter_by(user_id=current_user.id).all()
+
+        serialized_decks = {}
+        for deck in decks:
+            serialized_decks[deck.name] = deck.serialize()
+
+        return render_template(
+            "decks.html", decks=decks, serialized_decks=serialized_decks
+        )
     form = SignUpForm()
     return render_template("signup.html", form=form)
 
